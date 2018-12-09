bac7192a0fd0a6bba9a2e4230fbd621877c8fb1b
carter.ho <carter.go@gosmio.biz>
2018-12-09 15:34:54 +0800

    [Added] Init

---
+++ KafkaSignalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KafkaSignalR.WebApiApplication" Language="C#" %>
---
+++ KafkaSignalR/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿
+namespace KafkaSignalR
+{
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
